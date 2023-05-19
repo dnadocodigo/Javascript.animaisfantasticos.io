ef5094940de53714b20e7ce9493edd04fc0c517e
Paulo Galdino <dnadevnet@gmail.com>
2023-05-19 11:13:12 -0300

    add package-lock

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-import initActiveTabs from './modules/tabs_data-anime.js';
-import initAccordionList from './modules/accordion_data-anime.js';
-import initSoftScroll from './modules/soft_scroll_data-anime.js';
-import initSoftScrollAnimation from './modules/soft_scroll_animation_data-anime.js';
-import initModal from './modules/modal_data-modal.js';
-import initTooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/menu_dropdown.js';
-import initMenuMobile from './modules/menu_moblie.js';
-import initAnimeNumber from './modules/anime_number.js';
-import initFuncionamento from './modules/funcionamento.js';
-import initNumberFetchAnimals from './modules/number_animals_fetch.js';
-import initFetchBitcoin from './modules/bitcon_fetch.js';
+import initActiveTabs from "./modules/tabs_data-anime.js";
+import initAccordionList from "./modules/accordion_data-anime.js";
+import initSoftScroll from "./modules/soft_scroll_data-anime.js";
+import initSoftScrollAnimation from "./modules/soft_scroll_animation_data-anime.js";
+import initModal from "./modules/modal_data-modal.js";
+import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/menu_dropdown.js";
+import initMenuMobile from "./modules/menu_moblie.js";
+import initAnimeNumber from "./modules/anime_number.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initNumberFetchAnimals from "./modules/number_animals_fetch.js";
+import initFetchBitcoin from "./modules/bitcon_fetch.js";
 
 initActiveTabs();
 initAccordionList();
@@ -22,7 +22,7 @@ initMenuMobile();
 initDropdownMenu();
 initAnimeNumber();
 initFuncionamento();
-initNumberFetchAnimals()
+initNumberFetchAnimals();
 initFetchBitcoin();
 
 
